d77c2bbe590b7a4ba4150e358a91b4797e3f6f9b
HHJ <449216786@qq.com>
2024-06-14 09:35:38 +0800

    tSNE可视化代码

--- tSNE.py
+++ tSNE.py
@@ -4,6 +4,8 @@ import torchvision.transforms as transforms
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import numpy as np
+from data.data_manager import process_gallery_sysu
+from data.dataloader import TestData
 
 # torch.backends.cuda.max_split_size_mb = 2750
 
@@ -20,28 +22,6 @@ def get_cluster_loader(dataset, batch_size, workers):
 def t_SNE(args,
         model,
         ):
-
-    normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    if args.dataset == 'sysu':
-        transform_train_rgb = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomGrayscale(p=0.5),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.Pad(10),
-            transforms.RandomCrop((args.img_h, args.img_w)),
-            transforms.ToTensor(),
-            normalizer,
-            transforms.RandomErasing(p=0.5)
-        ])
-        transform_train_ir = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.Pad(10),
-            transforms.RandomCrop((args.img_h, args.img_w)),
-            transforms.ToTensor(),
-            normalizer,
-            transforms.RandomErasing(p=0.5),
-        ])
 
 
     batch = args.stage2_ims_per_batch
@@ -83,24 +63,13 @@ def t_SNE(args,
     text_features_rgb = text_features_rgb.cpu().numpy()
     text_features_ir = text_features_ir.cpu().numpy()
 
-    # new_rgb = [text_features_rgb[:100] for _ in range(3)]
-    # new_rgb = np.concatenate(new_rgb, axis=0)
-    # new_ir = [text_features_ir[:100] for _ in range(3)]
-    # new_ir = np.concatenate(new_ir, axis=0)
 
     tsne = TSNE(n_components=2, init='pca', random_state=0)
-    X_tsne_rgb = tsne.fit_transform(text_features_rgb)
-    X_tsne_ir = tsne.fit_transform(text_features_ir)
+    all_features = np.concatenate((text_features_rgb, text_features_ir), axis=0)
+    X_tsne = tsne.fit_transform(all_features)
+    X_tsne_rgb = X_tsne[:len(text_features_rgb)]
+    X_tsne_ir = X_tsne[len(text_features_rgb):]
 
-    # X_tsne_rgb = tsne.fit_transform(new_rgb)
-    # X_tsne_ir = tsne.fit_transform(new_ir)
-
-    # 将两种文本特征的tSNE结果画在一张图上
-    # plt.figure(figsize=(10, 5))
-    # plt.scatter(X_tsne_rgb[:, 0], X_tsne_rgb[:, 1], c='r', label='rgb', marker='*')
-    # plt.scatter(X_tsne_ir[:, 0], X_tsne_ir[:, 1], c='b', label='ir')
-    # plt.legend()
-    # plt.show()
 
     # 创建一个颜色列表，长度与特征点数量相同
     colors = plt.cm.rainbow(np.linspace(0, 1, len(text_features_rgb)))
@@ -111,7 +80,48 @@ def t_SNE(args,
     plt.scatter(X_tsne_rgb[:, 0], X_tsne_rgb[:, 1], c=colors, label='rgb', marker='*')
 
     # 对于ir特征，使用默认的圆形形状，颜色从颜色列表中获取
-    plt.scatter(X_tsne_ir[:, 0], X_tsne_ir[:, 1], c=colors, label='ir')
+    plt.scatter(X_tsne_ir[:, 0], X_tsne_ir[:, 1], c=colors, label='ir', marker='s')
+
+    plt.legend()
+    plt.show()
+    print('t-SNE finished!')
+
+def t_SNE_img(args,
+        model,
+        ):
+    normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transform_test = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((args.img_h, args.img_w)),
+        transforms.ToTensor(),
+        normalizer,
+    ])
+    gall_img, gall_label, gall_cam = process_gallery_sysu(args.data_path, mode=args.mode, trial=0)
+    gallset = TestData(gall_img, gall_label, transform=transform_test, img_size=(args.img_w, args.img_h))
+    gall_loader = data.DataLoader(gallset, batch_size=args.test_batch_size, shuffle=False, num_workers=args.workers)
+
+
+    text_features_rgb = text_features_rgb.cpu().numpy()
+    text_features_ir = text_features_ir.cpu().numpy()
+
+
+    tsne = TSNE(n_components=2, init='pca', random_state=0)
+    all_features = np.concatenate((text_features_rgb, text_features_ir), axis=0)
+    X_tsne = tsne.fit_transform(all_features)
+    X_tsne_rgb = X_tsne[:len(text_features_rgb)]
+    X_tsne_ir = X_tsne[len(text_features_rgb):]
+
+
+    # 创建一个颜色列表，长度与特征点数量相同
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(text_features_rgb)))
+
+    plt.figure(figsize=(10, 5))
+
+    # 对于rgb特征，使用五角星形状，颜色从颜色列表中获取
+    plt.scatter(X_tsne_rgb[:, 0], X_tsne_rgb[:, 1], c=colors, label='rgb', marker='*')
+
+    # 对于ir特征，使用默认的圆形形状，颜色从颜色列表中获取
+    plt.scatter(X_tsne_ir[:, 0], X_tsne_ir[:, 1], c=colors, label='ir', marker='s')
 
     plt.legend()
     plt.show()
